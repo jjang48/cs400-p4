102c2e80cd8c641f0b02c424c12f62baf91216e9
Bryan <bjin23@wisc.edu>
2018-04-11 11:52:11 -0500

    Added file header.

--- Graph.java
+++ Graph.java
@@ -1,12 +1,35 @@
 import java.util.HashSet;
 import java.util.Set;
+///////////////////////////////// FILE HEADER //////////////////////////////////
+//
+// Assignment name: DictionaryGraph
+//
+// Files submitted: Graph.java, GraphTest.java, WordProcessor.java,
+// GraphProcessor.java, GraphProcessorTest.java
+//
+// Course: CS 400 Spring 2018
+//
+// Authors: Bryan Jin, Joon Jang, Aanjanaye Kajaria
+//
+// Emails: bjin23@wisc.edu, jjang48@wisc.edu, kajaria@wisc.edu
+//
+// Lecturer's Name: Deb Deppeler
+//
+// Outside sources:
+// https://www.sitepoint.com/implement-javas-equals-method-correctly/ for help
+// implementing equals method of GraphVertex properly (I had forgotten that I
+// could use getClass() to safely cast the Object parameter).
+//
+// https://coderanch.com/t/572755/certification/
+// HashSet-adding-duplicates-hashcode-obects
+// for helping me realize that I needed to override the hashCode() method for
+// GraphVertex in order to properly use HashSet.
+//
+// Known bugs: NONE
+//
+// Due date: 4/16/2018
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 
-// Sources: https://www.sitepoint.com/implement-javas-equals-method-correctly/ for help
-// implementing equals method of GraphVertex properly (I had forgotten that I could use getClass()
-// to safely cast the Object parameter)
-// https://coderanch.com/t/572755/certification/HashSet-adding-duplicates-hashcode-obects for
-// helping me realize that I needed to override the hashCode() method for GraphVertex in order
-// to properly use HashSet
 
 /**
  * Undirected and unweighted graph implementation
